da9a8aa29e2f7b92300211ee4805a9c6100fc739
Luis <gonzalez.engineer.com>
2025-09-08 00:12:48 -0400

    hello world (python)

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+print("Self " + "School")
+print("Python " + "is " + "fun")
+print("Python" + "3")
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Selfschool"
+print(str[0:4] + str[4:6] + str[6:8])
